df2ecb15a8f85fe761259a89d0416767c7be3fc7
Elham <139170682+EluFeR@users.noreply.github.com>
2026-04-06 10:49:42 +0300

    Moved frontend files into frontend directory

--- frontend/citizen/src/components/layout/StatusBadge.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "../../styles/StatusBadge.css";
-
-const StatusBadge = ({ status }) => {
-  return <span className={`badge ${status.toLowerCase()}`}>{status}</span>;
-};
-
-export default StatusBadge;
